c92016de68b307b219d0784087caec61acd187e0
Matt Lombana <mjlombana@icloud.com>
2018-05-26 21:27:47 -0400

    Stop services when the ips are the same

--- IP-Change-Text/ip_change_text.sh
+++ IP-Change-Text/ip_change_text.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 HOME=$(dig +short myip.opendns.com @resolver1.opendns.com)
 PUBLIC_URL=$(cat .Public_URL)
 PUBLIC=$(dig +short $PUBLIC_URL)
@@ -6,6 +8,8 @@ PHONE=$(cat .Phone_Number)
 
 if [[ "$HOME" = "$PUBLIC" ]]; then
 	curl http://textbelt.com/text -d number=$PHONE -d message='The IP Addresses are the same' -key -d key=$KEY > /dev/null 2>&1
+    systemctl stop sabnzbd
+	systemctl stop deluged
 fi
 
 
