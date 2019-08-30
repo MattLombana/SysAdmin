be8bf13e6d8186426fba62741ddb514442a93636
Matt Lombana <mjlombana@icloud.com>
2019-08-29 21:58:48 -0500

    Add ability to customize header height, and set the default smaller

--- Utilities/heading_generator.py
+++ Utilities/heading_generator.py
@@ -3,9 +3,9 @@
 import argparse
 
 
-def make_header(width, commentchar, space, line):
+def make_header(width, height, commentchar, space, line):
     endlines = commentchar * width
-    blanklines = '{0}{1}{0}'.format(commentchar, space * (width - 2))
+    blanklines = '{0}{1}{0}\n'.format(commentchar, space * (width - 2))
 
 
     neededspace = width - 2 - len(line)
@@ -16,15 +16,22 @@ def make_header(width, commentchar, space, line):
     else:
         rightcenterspace = leftcenterspace + 1
 
+    num_blanklines = (height - 3) // 2
 
+    print('num_blanklines {}'.format(num_blanklines))
 
     titleline = '{0}{1}{2}{3}{0}'.format(commentchar, space * leftcenterspace, line, space * rightcenterspace)
-    print('{0}\n{1}\n{2}\n{1}\n{0}'.format(endlines, blanklines, titleline))
+    print(endlines)
+    print(blanklines * num_blanklines, end = '')
+    print(titleline)
+    print(blanklines * num_blanklines, end = '')
+    print(endlines)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Create a nice markdown heading')
-    parser.add_argument('-w', help='how many characters wide', type=int, default=100)
+    parser.add_argument('-w', help='How many characters wide', type=int, default=100)
+    parser.add_argument('-t', help='How many lines tall', type=int, default='3')
     parser.add_argument('-s', help='Character for spacing comments', default=' ')
     parser.add_argument('-c', help='Character for denoting comments', default='#')
     parser.add_argument('lines', help='Strings to make into header', nargs='+')
@@ -32,12 +39,13 @@ def main():
     args = parser.parse_args()
 
     width = args.w  # width
+    height = args.t  # height
     commentchar = args.c  # commentchar
     space = args.s  # space
     lines = args.lines
 
     for line in lines:
-        make_header(width, commentchar, space, line)
+        make_header(width, height, commentchar, space, line)
         print('\n')
 
 
